7711996ddcaf14de4fecf7d20af3e2f0a51890c6
Rayhan F <rayhanfrdh123@gmail.com>
2024-09-03 02:33:17 +0700

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 export default function Footer() {
+  let currentYear = new Date().getFullYear();
+
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">&copy; {2024 == currentYear ? "2024" : `2024 - ${currentYear}`} Rayhan F. All rights reserved.</small>
       <small className="mb-2 block text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
+        Inspired by{" "}
+        <a href="https://github.com/ByteGrad/portfolio-website" className="text-slate-700">
+          Ricardo
+        </a>
       </small>
-      <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
-      </p>
     </footer>
   );
 }
