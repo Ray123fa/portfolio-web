20de6571c59bcbf76cea6cd90186c9baa9d3a14e
Rayhan F <rayhanfrdh123@gmail.com>
2024-11-21 22:51:03 +0700

    update: comment out CV download link in intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -57,9 +57,9 @@ export default function Intro() {
           delay: 0.1,
         }}
       >
-        <a className="group bg-gray-900 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-105 transition cursor-pointer borderBlack hover:bg-gray-950" href="#">
+        {/* <a className="group bg-gray-900 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-105 transition cursor-pointer borderBlack hover:bg-gray-950" href="#">
           Download CV <HiDownload className="opacity-60 transition" />
-        </a>
+        </a> */}
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
