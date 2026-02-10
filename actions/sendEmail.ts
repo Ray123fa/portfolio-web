7150b9a4e0a45872040babb2eb6efb66da6d2a93
Rayhan F <rayhanfrdh123@gmail.com>
2026-02-11 05:36:41 +0700

    feat: configure contact form email with custom domain

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,8 +26,8 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
-      to: "bytegrad@gmail.com",
+      from: "Rayfa Portfolio <noreply@rayfa.my.id>",
+      to: "contact@rayfa.my.id",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
